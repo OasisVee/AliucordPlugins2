2b0b1650e55dc2031f5faeef789c09a7f3e1b178
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-04-26 03:31:29 -0400

    Update AnimateApngs.java

--- AnimateApngs/src/main/java/dev/vendicated/aliucordplugs/animateapngs/AnimateApngs.java
+++ AnimateApngs/src/main/java/dev/vendicated/aliucordplugs/animateapngs/AnimateApngs.java
@@ -14,41 +14,20 @@ import android.content.Context;
 import android.net.Uri;
 import android.widget.ImageView;
 
+import com.aliucord.Http;
+import com.aliucord.Utils;
 import com.aliucord.annotations.AliucordPlugin;
 import com.aliucord.entities.Plugin;
-import com.aliucord.patcher.*;
-
-import com.aliucord.Http;
-import com.aliucord.Logger;
-import com.aliucord.Utils;
-
-import com.aliucord.utils.ReflectUtils;
-import com.discord.widgets.chat.input.WidgetChatInputAttachments;
-import com.discord.widgets.chat.input.WidgetChatInputAttachments$createAndConfigureExpressionFragment$stickerPickerListener$1;
-import com.discord.widgets.chat.input.sticker.*;
-import com.discord.utilities.stickers.StickerUtils;
-import com.discord.utilities.rest.RestAPI;
-import com.discord.restapi.RestAPIParams;
-import com.discord.models.domain.NonceGenerator;
-import com.discord.utilities.time.ClockFactory;
-import com.aliucord.utils.RxUtils;
-import java.util.Collections;
-import com.discord.stores.StoreStream;
-
-// APNG Animation support imports
+import com.aliucord.patcher.Hook;
 import com.discord.api.message.embed.EmbedType;
 import com.discord.embed.RenderableEmbedMedia;
 import com.discord.widgets.chat.list.InlineMediaView;
 import com.discord.widgets.media.WidgetMedia;
+
 import java.util.regex.Pattern;
 
-@SuppressWarnings("unused")
 @AliucordPlugin
-public class FakeStickers extends Plugin {
-    private final Logger logger = new Logger("FakeStickers");
-
-    public FakeStickers() {}
-
+public class AnimateApngs extends Plugin {
     private void initApng(ImageView view, String mediaUrl, Integer w, Integer h) {
         final var url = mediaUrl
                 // Strip proxy but only if they're Discord domains.
@@ -58,7 +37,7 @@ public class FakeStickers extends Plugin {
 
         Utils.threadPool.execute(() -> {
             try (var is = new Http.Request(url).execute().stream()) {
-                // You cannot consume the stream twice so just yolo it and don't use Apng.Companion.isApng first
+                // You cannot consume the stream twice so just yolo it and don't use Apng.Companion.isApng first, eh whatever.
                 var drawable = b.l.a.a.a(is, w, h);
                 if (view != null)
                     Utils.mainThread.post(() -> {
@@ -70,63 +49,10 @@ public class FakeStickers extends Plugin {
     }
 
     @Override
-    public void start(Context context) throws Throwable {
-        // --- FAKE STICKERS FUNCTIONALITY ---
-        
-        // Do not mark stickers as unsendable (grey overlay)
-        patcher.patch(StickerItem.class.getDeclaredMethod("getSendability"), InsteadHook.returnConstant(StickerUtils.StickerSendability.SENDABLE));
-
-        // Patch onClick to send sticker
-        patcher.patch(WidgetStickerPicker.class.getDeclaredMethod("onStickerItemSelected", StickerItem.class), new PreHook(param -> {
-            try {
-                // getSendability is patched above to always return SENDABLE so get the real value via reflect
-                if (ReflectUtils.getField(param.args[0], "sendability") == StickerUtils.StickerSendability.SENDABLE) return;
-
-                var sticker = ((StickerItem) param.args[0]).getSticker();
-
-                RestAPIParams.Message message = new RestAPIParams.Message(
-                    "https://media.discordapp.net/stickers/"+sticker.d()+sticker.b()+"?size=160",
-                    Long.toString(NonceGenerator.computeNonce(ClockFactory.get())),
-                    null,
-                    null,
-                    Collections.emptyList(),
-                    null,
-                    new RestAPIParams.Message.AllowedMentions(
-                            Collections.emptyList(),
-                            Collections.emptyList(),
-                            Collections.emptyList(),
-                            false
-                    ),
-                    null,
-                    null
-                );
-                logger.debug(message.toString());
-                Utils.threadPool.execute(() -> {
-                    // Subscriptions in Java, because you can't do msg.subscribe() like in Kotlin
-                    RxUtils.subscribe(
-                            RestAPI.getApi().sendMessage(StoreStream.getChannelsSelected().getId(), message),
-                            RxUtils.createActionSubscriber(zz -> {})
-                    );
-                });
-
-                // Skip original method
-                param.setResult(null);
-
-                // Dismiss sticker picker
-                var stickerListener = (WidgetChatInputAttachments$createAndConfigureExpressionFragment$stickerPickerListener$1)
-                        ReflectUtils.getField(param.thisObject, "stickerPickerListener");
-                WidgetChatInputAttachments.access$getFlexInputFragment$p(stickerListener.this$0).s.hideExpressionTray();
-            } catch (Throwable e) {
-                logger.error("Error in sticker patch", e);
-            }
-        }));
-
-        // --- ANIMATE APNGS FUNCTIONALITY ---
-        
+    public void start(Context ctx) throws Throwable {
         int previewResId = Utils.getResId("inline_media_image_preview", "id");
         int mediaResId = Utils.getResId("media_image", "id");
 
-        // Patch InlineMediaView to handle APNGs
         var updateUI = InlineMediaView.class.getDeclaredMethod("updateUI", RenderableEmbedMedia.class, String.class, EmbedType.class, Integer.class, Integer.class, String.class);
         patcher.patch(updateUI, new Hook(param -> {
             var media = (RenderableEmbedMedia) param.args[0];
@@ -139,7 +65,7 @@ public class FakeStickers extends Plugin {
             initApng(view, url, media.b, media.c);
         }));
 
-        // Patch WidgetMedia to handle APNGs in full view
+
         var pattern = Pattern.compile("\\.png(?:\\?width=(\\d+)&height=(\\d+))?");
 
         var uriField = WidgetMedia.class.getDeclaredField("imageUri");
@@ -159,15 +85,12 @@ public class FakeStickers extends Plugin {
                     var view = (ImageView) WidgetMedia.access$getBinding$p(widgetMedia).getRoot().findViewById(mediaResId);
                     initApng(view, url, w != null ? Integer.parseInt(w) : null, h != null ? Integer.parseInt(h) : null);
                 }
-            } catch (Throwable e) {
-                logger.error("Error in APNG widget media patch", e);
-            }
+            } catch (Throwable ignored) { }
         }));
     }
 
     @Override
     public void stop(Context context) {
-        // Remove all patches
         patcher.unpatchAll();
     }
-                 }
+}
